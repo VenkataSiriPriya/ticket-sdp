3fcd56e61db24cdb4f8242ec5649f703be8b3106
VenkataSiriPriya <2300030401@kluniversity.in>
2025-11-27 23:19:33 +0530

    ticket-booking

--- SDP-CICD-F/src/customer/CustomerNavBar.jsx
+++ SDP-CICD-F/src/customer/CustomerNavBar.jsx
@@ -27,7 +27,7 @@ export default function CustomerNavBar()
           <li><Link to="/customerhome">Home</Link></li>
           <li><Link to="/customerprofile">Customer Profile</Link></li>
           <li><Link to="/updateprofile">Update Profile</Link></li>
-          <li><Link to="/viewallevents">Book a New Event</Link></li>
+          <li><Link to="/viewallevents">Book New Event</Link></li>
           <li><Link to="/bookedevents">Booked Events</Link></li>
           <li><Link to="/customerlogin" onClick={handleLogout}>Logout</Link></li>
         </ul>
